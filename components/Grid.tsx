4b8bcd88d44d179f4065c2869f59f7d5acb51487
Angel Aguirre Obando <anngelao25@gmail.com>
2024-06-10 23:23:38 -0500

    Added BentoGrid component

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid"
+import { gridItems } from '@/data'
+
 
 const Grid = () => {
-  return <div>Grid</div>;
+  return <section id="about"><BentoGrid>
+  {gridItems.map 
+    ((item, i) => (
+    <BentoGridItem
+          id={item.id}
+          key={item.id}
+          title={item.title}
+          description={item.description}
+        />
+    ))}</BentoGrid></section>;
 };
 
 export default Grid;
---
+++ data/index.ts
@@ -0,0 +1,6 @@
+export const gridItems = [
+  { title: 'Title 1', description: 'Desc1', id: 1 },
+  { title: 'Title 1', description: 'Desc1', id: 1 },
+  { title: 'Title 1', description: 'Desc1', id: 1 },
+  { title: 'Title 1', description: 'Desc1', id: 1 }
+]
